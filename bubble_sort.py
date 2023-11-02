479da87843e9e4d5a8ec9ffd1d3246a0aa3bf302
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2023-11-02 12:15:03 -0700

    Added the copyright notice

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,3 +1,17 @@
+'''
+# ******************************************************************************
+# *                               COPYRIGHT NOTICE                             *
+# ******************************************************************************
+# *                                                                            *
+# *  This code is authored by Marcelo Villalobos Diaz                          *
+# *  You are free to use, modify, and distribute this code, provided           *
+# *  you give appropriate credit by including the author's name.               *
+# *                                                                            *
+# *  Copyright (c) 2023 Marcelo Villalobos Diaz                                *
+# *                                                                            *
+# ******************************************************************************
+'''
+
 import random
 
 mySecondFile = open("somefile.txt", 'w')
